d4f4025e9d423bbef0aea7de3497d2d577c84598
amine <bouaziz.amine.18@gmail.com>
2018-11-24 14:34:26 +0100

    Ionic-geolocalisation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController,Platform } from 'ionic-angular';
+import { Geolocation,GeolocationOptions,Geoposition } from '@ionic-native/geolocation';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  geoposition :Geoposition;
+
+
+  constructor(private platform: Platform,
+    private geolocation:Geolocation,
+    public navCtrl: NavController) {
 
   }
-
+ async getGeolocation(){
+     await this.platform.ready();
+    try{
+      this.geoposition = await this.geolocation.getCurrentPosition();
+    }
+    catch(e){
+      console.error(e);
+    }
+    }
 }
